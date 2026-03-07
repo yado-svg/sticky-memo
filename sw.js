083d39fc5cd82b304814a06ed86564976ae79c27
yado-svg <l96yado@gmail.com>
2026-03-07 10:42:07 +0900

    AQSEE Memo PWA

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='smv4';self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html','./manifest.json'])));self.skipWaiting()});self.addEventListener('activate',e=>e.waitUntil(clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>null))));
